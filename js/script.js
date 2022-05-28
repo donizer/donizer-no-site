2ca4e335446e31fb6927bcdd96b8e87b43033bbc
donizer <bogdan.ua45@gmail.com>
2022-05-28 21:41:31 +0300

    v pizdu

--- js/script.js
+++ js/script.js
@@ -17,3 +17,21 @@ $(document).ready(function(){
 $('.header__burger').css({'cursor': 'pointer'});
 
 
+
+// var golo = 'https://api-cdn.rule34.xxx/samples/5384/sample_d3c7ccd8a3d5c7ef240549aab4484070.jpg'
+
+// // var golo = golo.slice(16)
+
+// // for(let x = 0; x < golo.length; x++){
+// //     console.log(golo[x])
+// // }
+// // console.log(golo)
+
+// var golo = `https://${golo.slice(16)}`;
+
+
+// var elements = document.getElementsByClassName("ad");
+
+// window.onclick = function(event){
+    
+// }
--- js/test.js
+++ js/test.js
@@ -1,152 +1,134 @@
 // var rerequestURL = `https://r34-json-api.herokuapp.com/posts?tags=ahri`
 var rerequestURL = `https://api.rule34.xxx/index.php?page=dapi&s=post&q=index&json=1`
-// var request = new XMLHttpRequest();
-// request.open('GET', rerequestURL)
-// request.responseType = 'json';
-// request.send();
+var request = new XMLHttpRequest();
+request.open('GET', rerequestURL)
+request.responseType = 'json';
+request.send();
 
+var testurl = "https://api-cdn.rule34.xxx/samples/5384/sample_d3c7ccd8a3d5c7ef240549aab4484070.jpg";
+// var testurl = "https://cdn.discordapp.com/attachments/497404476907454466/978986951120154674/unknown.png";
 
 var maxValue = 100;
 
 
-for(let x = 0; x < maxValue; x++){
-    var div_card_body = 
-    $("<div>", {id: `r34obj${x}`, class: "card__body r34"}).append(
-        $("<a>", {class: "card__image"}).append(
-            $("<img>", {id: `r34img${x}`,
-            class: "card__image-src lazyload"})
-            .append(
-                
-            )
-        )
-    );
-    
-    $('#CardContainer').append(div_card_body);
-}
 
-window.onload = function() {
+
+$(document).ready(function(){
+
+	
+	
 	var isVideo = false;
-    $.getJSON(rerequestURL, function(data){
-        
-		for(let x = 0; x < maxValue; x++){
 
-			var tagString = data[x].tags
-			var tagsАrray = tagString.split(' ');
-			
-			$(`#r34img${x}`).attr("src", `${data[x].preview_url}`);
-
-			for(let y = 0; y < tagsАrray.length; y++){
-				// console.log(`tags of ${x} is ${tagsАrray[y]}`);
-				
-				if(tagsАrray[y] == "animated"){
-					$(`#r34img${x}`).toggleClass('anim');
-
-				}
-				
-				// console.log();
-
-			}
-		}
-        
-		// console.log(data)
-
-
-
-		$('.card__image-src').click(function(event){
+	for(let x = 0; x < maxValue; x++){
+		var div_card_body = 
+		$("<div>", {id: `r34obj${x}`, class: "card__body r34"}).append(
+			$("<a>", {class: "card__image"}).append(
+				$("<img>", {id: `r34img${x}`,
+				class: "card__image-src lazyload"})
+				.append(
+					
+				)
+			)
+		);
 		
-			$('.card__body, .r34previewer')
-			.toggleClass('active');
-			$('body')
-			.toggleClass('lock');
-	
-			var r34id = event.target.id.slice(6);
-	
-			var r34URLMicro = data[r34id].preview_url;
-			var r34URLSimple = data[r34id].sample_url;
-			var r34URLFull = data[r34id].file_url;
+		$('#CardContainer').append(div_card_body);
+	}
+
+	request.onload = function() {
+		var r34data = request.response;
+
+		// console.log(r34data);
+		for(let x = 0; x < maxValue; x++){
 			
-			var testurl = "https://api-cdn.rule34.xxx/samples/5384/sample_d3c7ccd8a3d5c7ef240549aab4484070.jpg";
-	
-			var imageURL = r34URLFull;
-	
-			// request.open('GET', imageURL)
-			// request.responseType = 'blob';
-			// request.send();
-	
-			// console.log(imageURL);
+			$(`#r34img${x}`).attr('src', r34data[x].preview_url);
+			
+		}
+
+		$('.card__image-src, .r34previewer').click(function(){
+
+			if (this.id != 'r34previewer') {
+
+				var r34id = this.id.slice(6)
 
 
-			let last4CharURL = r34URLFull.slice(-3)
-			let isMp4 = false;
-			if(last4CharURL == 'mp4'){
-				isMp4 = true;
+				var type,
+					r34Folder,
+					r34Hash,
+					r34Image,
+					r34ImageArray,
+					r34Format,
+					r34ID;
+
+				if(r34data[r34id].sample == 1) {
+
+					type = 'samples';
+					r34Folder = r34data[r34id].directory;
+					r34Hash = `sample_${r34data[r34id].hash}`;
+					r34ImageArray = r34data[r34id].image.split('.');
+					r34Image = r34ImageArray[0];
+					r34Format = 'jpg'
+					r34ID = r34data[r34id].id;
+
+
+					var imageURL = `https://rule34.xxx//${type}/${r34Folder}/${r34Hash}.${r34Format}?${r34ID}`
+					
+				} else if (r34data[r34id].sample == 0) {
+
+					type = 'images';
+					r34Folder = r34data[r34id].directory;
+					r34ImageArray = r34data[r34id].image.split('.');
+					r34Image = r34ImageArray[0];
+					r34Format = r34ImageArray[r34ImageArray.length-1];
+					r34ID = r34data[r34id].id;
+
+
+					var imageURL = `https://rule34.xxx//${type}/${r34Folder}/${r34Image}.${r34Format}?${r34ID}`
+					
+				}
+
+				// var sampleURL = `https://rule34.xxx/${r34data[r34id].file_url.slice(26)}?${r34data[r34id].id}`
+				// var sampleURL = r34data[r34id].sample_url.slice(26);
+
+				var div_prviewer = 
+				$("<img>", {
+					id: "r34previewer-img",
+					class: "r34previewer-img lazyload",
+					src: '#'
+				});
+		
+				$('#mediaPreviewer').append(div_prviewer);
+
+				$('#r34previewer-img').attr('src', imageURL);
+
+
+
+				console.log(imageURL)
+				console.log(r34ImageArray)
+				console.log(r34Format)
+		
+			} else if (this.id == 'r34previewer') {
+				console.log(this.id)
+		
+				$('#r34previewer-img').remove();
 			}
+	
+/*
+     https://rule34.xxx//images/5399/d92f8ff63addc57075869810e2038e79.jpeg?6149497
+https://wimg.rule34.xxx//images/5399/d92f8ff63addc57075869810e2038e79.jpeg?6149497
 
-			// console.log(last4CharURL);
+     https://rule34.xxx//images/5399/19dc0a32d175f59aad8e7f6f3cbe1fd3.jpeg?6149489
+https://wimg.rule34.xxx//images/5399/19dc0a32d175f59aad8e7f6f3cbe1fd3.jpeg?6149489
 
-			var addVideo = 
-			$("<video>", {id: "r34previewer-vid", class: "r34previewer-vid", src: `${imageURL}`, autoplay: '1', loop: '1'});
+https://rule34.xxx//samples/5400/sample_5faa2c8386bd9a47dd8cb1f72fe2d492.jpg?6150408
+https://rule34.xxx//samples/5400/sample_5faa2c8386bd9a47dd8cb1f72fe2d492.jpg?6150408
 
-			var addImage = 
-			$("<img>", {id: "r34previewer-img", class: "r34previewer-img", src: `${r34URLSimple}`});
-
-			
-
-			if(isMp4){
-				// $('#r34previewer-vid').attr('src', `${imageURL}`)
-				console.log(addVideo)
-				$('#r34previewer').append(addVideo);
-				isVideo = true;
-				addVideo = '';
-				console.log(addVideo)
-			} else {
-				$('#r34previewer').append(addImage);
-				isVideo = false;
-				addImage = '';
-			}
-
-
-			
-
-			// console.log(testurl);
-			// console.log(r34URLMicro);
-			// console.log(r34URLSimple);
-			// console.log(r34URLFull);
-				
+*/
+	
+			$('.r34previewer').toggleClass('active');
+			$('body').toggleClass('lock');	
+	
 		});
 
-
-		
-    }).fail(function(){
-        console.log("An error has occurred.");
-    });
-
-	$('#r34previewer').click(function(event){
-
-		
-		// var evenVid = document.getElementsByClassName('r34previewer')[0];
-		// console.log(isVideo);
-		// console.log(!!evenVid.children.length);
-
-		if(isVideo){
-			document.getElementById('r34previewer-vid').remove();
-		} else {
-			document.getElementById('r34previewer-img').remove();
-		}
-
-		// console.log('123')
+	}
 	
-		$('.card__body, .r34previewer')
-		.toggleClass('active');
-		$('body')
-		.toggleClass('lock');
-	
-		// $(`#r34previewer-img`).attr("src", '../media/images/placeholder.svg');
-		// $(`#r34previewer-vid`).attr("src", '#');
-		// $(`#r34previewer-vid`).get(0).pause();
-	
-	});
-	
-
-
-}
+}); 
