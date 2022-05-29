aba0f3f467268aa7385b9eba2c5d7a6f4fc5d462
donizer <bogdan.ua45@gmail.com>
2022-05-29 14:14:34 +0300

    video try

--- js/test.js
+++ js/test.js
@@ -1,5 +1,5 @@
 // var rerequestURL = `https://r34-json-api.herokuapp.com/posts?tags=ahri`
-var rerequestURL = `https://api.rule34.xxx/index.php?page=dapi&s=post&q=index&json=1`
+var rerequestURL = `https://api.rule34.xxx/index.php?page=dapi&s=post&q=index&json=1&tags=mp4`
 var request = new XMLHttpRequest();
 request.open('GET', rerequestURL)
 request.responseType = 'json';
@@ -37,74 +37,108 @@ $(document).ready(function(){
 	request.onload = function() {
 		var r34data = request.response;
 
+
 		// console.log(r34data);
-		for(let x = 0; x < maxValue; x++){
+
+
+
+		var r34Image,
+			r34ImageArray,
+			r34Format;
+		for (let x = 0; x < maxValue; x++) {
 			
+			
+			
+
+			r34Image = r34data[x].image;
+			r34ImageArray = r34Image.split('.');
+			r34Format = r34ImageArray[r34ImageArray.length-1];
+
+			if(r34Format == "mp4" || r34Format == "gif"){
+				console.log(`format is ${r34Format}`)
+				$(`#r34img${x}`).toggleClass('anim');
+
+			}
+
 			$(`#r34img${x}`).attr('src', r34data[x].preview_url);
 			
 		}
+
 
 		$('.card__image-src, .r34previewer').click(function(){
 
 			if (this.id != 'r34previewer') {
 
-				var r34id = this.id.slice(6)
-
+				var thisId = this.id.slice(6)
 
 				var type,
-					r34Folder,
-					r34Hash,
-					r34Image,
-					r34ImageArray,
-					r34Format,
-					r34ID;
+					r34Folder = r34data[thisId].directory,
+					r34ImageArray = r34data[thisId].image.split('.'),
+					r34Image = r34ImageArray[0],
+					r34Format = r34ImageArray[r34ImageArray.length-1],
+					r34ID = r34data[thisId].id;
 
-				if(r34data[r34id].sample == 1) {
-
-					type = 'samples';
-					r34Folder = r34data[r34id].directory;
-					r34Hash = `sample_${r34data[r34id].hash}`;
-					r34ImageArray = r34data[r34id].image.split('.');
-					r34Image = r34ImageArray[0];
-					r34Format = 'jpg'
-					r34ID = r34data[r34id].id;
-
-
-					var imageURL = `https://wimg.rule34.xxx//${type}/${r34Folder}/${r34Hash}.${r34Format}?${r34ID}`
-					
-				} else if (r34data[r34id].sample == 0) {
-
+				
+				if(r34Format == 'mp4'){
 					type = 'images';
-					r34Folder = r34data[r34id].directory;
-					r34ImageArray = r34data[r34id].image.split('.');
-					r34Image = r34ImageArray[0];
-					r34Format = r34ImageArray[r34ImageArray.length-1];
-					r34ID = r34data[r34id].id;
+
+					// var imageURL = `https://ws-cdn-video.rule34.xxx//${type}/${r34Folder}/${r34Image}.${r34Format}`
+					// var imageURL = r34data[thisId].file_url;
+					var imageURL = 'https://rule34.xxx/images/5402/2a0d1ec831997dbd60618dfae9d52002.mp4?6153181';
+
+/* 
+https://ws-cdn-video.rule34.xxx//images/5402/2a0d1ec831997dbd60618dfae9d52002.mp4?6153181
+https://ws-cdn-video.rule34.xxx//images/5402/2a0d1ec831997dbd60618dfae9d52002.mp4?6153181
 
 
-					var imageURL = `https://wimg.rule34.xxx//${type}/${r34Folder}/${r34Image}.${r34Format}?${r34ID}`
-					
-				}
+*/
 
-				// var sampleURL = `https://rule34.xxx/${r34data[r34id].file_url.slice(26)}?${r34data[r34id].id}`
-				// var sampleURL = r34data[r34id].sample_url.slice(26);
+					var div_prviewer = 
+					$("<video>", {
+						id: "r34previewer-img",
+						class: "r34previewer-img",
+						src: imageURL,
+						controls: "controls"
+					});
+					$('#mediaPreviewer').append(div_prviewer);
 
-				var div_prviewer = 
-				$("<img>", {
-					id: "r34previewer-img",
-					class: "r34previewer-img lazyload",
-					src: '#'
-				});
-		
-				$('#mediaPreviewer').append(div_prviewer);
+					// $("#r34previewer-img").html(`<source src="${imageURL}" type="video/mp4"></source>`)
 
-				$('#r34previewer-img').attr('src', imageURL);
+				} else {
+					if(r34data[thisId].sample == 1) {
 
+						type = 'samples';
+						r34Format = 'jpg'
+	
+						var imageURL = `https://rule34.xxx//${type}/${r34Folder}/sample_${r34Image}.${r34Format}?${r34ID}`
+						
+					} else if (r34data[thisId].sample == 0) {
+	
+						type = 'images';
+	
+						if(r34Format == 'png' || r34Format == 'jpeg'){
+							var imageURL = `https://wimg.rule34.xxx//${type}/${r34Folder}/${r34Image}.${r34Format}?${r34ID}`
+						} else {
+							var imageURL = `https://rule34.xxx//${type}/${r34Folder}/${r34Image}.${r34Format}?${r34ID}`
+						}
+					}
 
+					var div_prviewer = 
+					$("<img>", {
+						id: "r34previewer-img",
+						class: "r34previewer-img lazyload",
+						src: imageURL
+					});
+					$('#mediaPreviewer').append(div_prviewer);
+				} 
+							
 
+	
+	
 				console.log(imageURL)
 				console.log(r34ImageArray)
 				console.log(r34Format)
+
 		
 			} else if (this.id == 'r34previewer') {
 				console.log(this.id)
@@ -113,14 +147,9 @@ $(document).ready(function(){
 			}
 	
 /*
-     https://rule34.xxx//images/5399/d92f8ff63addc57075869810e2038e79.jpeg?6149497
-https://wimg.rule34.xxx//images/5399/d92f8ff63addc57075869810e2038e79.jpeg?6149497
+	 https://rule34.xxx//images/5403/845824dba5547ae6c3eba3762fac6d8d.jpeg?6153622 я
+https://wimg.rule34.xxx//images/5403/845824dba5547ae6c3eba3762fac6d8d.jpeg?6153622 надо
 
-     https://rule34.xxx//images/5399/19dc0a32d175f59aad8e7f6f3cbe1fd3.jpeg?6149489
-https://wimg.rule34.xxx//images/5399/19dc0a32d175f59aad8e7f6f3cbe1fd3.jpeg?6149489
-
-https://rule34.xxx//samples/5400/sample_5faa2c8386bd9a47dd8cb1f72fe2d492.jpg?6150408
-https://rule34.xxx//samples/5400/sample_5faa2c8386bd9a47dd8cb1f72fe2d492.jpg?6150408
 
 */
 	
